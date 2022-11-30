d80cba2f6930defb31ad171896fd85e3a4a5066f
andre-usf <andrefretta@gmail.com>
2022-11-30 00:34:02 -0300

    Exclui comentários.

--- src/services/requestMealsAPI.js
+++ src/services/requestMealsAPI.js
@@ -1,21 +1,3 @@
-// export const getCategoryThemealdb = () => {
-//   const url = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list");
-//   const data = url.json();
-//   return data;
-// };
-
-// export const getNationalityThemealdb = () => {
-//   const url = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list");
-//   const data = url.json();
-//   return data;
-// };
-
-// export const getIngredientsThemealdb = () => {
-//   const url = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?i=list");
-//   const data = url.json();
-//   return data;
-// };
-
 const requestMealsAPI = async (filterOption, valueSearch) => {
   if (filterOption === 'ingredient-search') {
     try {
