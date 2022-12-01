2af25a6bd71c56923fb0b67d19249a3563398980
Matheus Bezerra <mabezerra2@gmail.com>
2022-12-01 00:51:18 -0400

    feat(component Recipes): Refatoração

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { RecipesLinks } from '../style/styled';
 
 export default function Recipes(props) {
   const { cardInfo, type, name, index, idRoute, title } = props;
 
   return (
+
     <Link to={ `/${title}/${cardInfo[idRoute]}` }>
-      <div data-testid={ `${index}-recipe-card` }>
-        <h4 data-testid={ `${index}-card-name` }>{cardInfo[name]}</h4>
+      <RecipesLinks data-testid={ `${index}-recipe-card` }>
         <img
           src={ cardInfo[type] }
           alt={ `${name}` }
           data-testid={ `${index}-card-img` }
         />
-
-      </div>
+        <h4 data-testid={ `${index}-card-name` }>{cardInfo[name]}</h4>
+      </RecipesLinks>
     </Link>
   );
 }
