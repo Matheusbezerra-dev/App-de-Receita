19126aaaedc326a1d3795bab32cd8a9af479a1f8
andre-usf <andrefretta@gmail.com>
2022-12-01 18:26:27 -0300

    Completa requisitos 57, 58 e 59.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,27 @@
+import React/* , { useContext } */ from 'react';
+import { Button } from 'react-bootstrap';
 import Header from './Header';
 import Footer from './Footer';
+// import AppContext from '../context/AppContext';
 
 export default function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <Header titlePage="Profile" buttonSearch={ false } />
+      <div>
+        <p data-testid="profile-email">{email}</p>
+        <p><Button type="button" data-testid="profile-done-btn">Done Recipes</Button></p>
+        <p>
+          <Button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </Button>
+        </p>
+        <p><Button type="button" data-testid="profile-logout-btn">Logout</Button></p>
+      </div>
       <Footer />
     </div>
   );
