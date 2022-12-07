379c1013045c5aa3c0bea05c4580593e08784f65
LucasCavalheri <cav.catecumenal@gmail.com>
2022-12-07 17:46:39 -0300

    Adiciona teste do alert

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -5,6 +5,10 @@ import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 
 describe('Testa o componente SearchBar', () => {
+  beforeEach(() => {
+    global.alert = jest.fn();
+  });
+
   test('Testa se consegue mudar o input radio', () => {
     const { history } = renderWithRouter(<App />);
     act(() => { history.push('/meals'); });
@@ -30,6 +34,10 @@ describe('Testa o componente SearchBar', () => {
     const { history } = renderWithRouter(<App />);
     act(() => { history.push('/meals'); });
 
+    const firstLetter = screen.getByTestId('first-letter-search-radio');
+    expect(firstLetter).toBeInTheDocument();
+    userEvent.click(firstLetter);
+
     const searchBtn = screen.getByTestId('button-search');
     expect(searchBtn).toBeInTheDocument();
     userEvent.click(searchBtn);
@@ -42,9 +50,26 @@ describe('Testa o componente SearchBar', () => {
     expect(execBtn).toBeInTheDocument();
     userEvent.click(execBtn);
 
-    // expect(window.alert).toBeInTheDocument();
+    expect(global.alert).toHaveBeenCalled();
   });
-  test('Testa se, ao encontrar um resultado válido, os elementos são exibidos na tela', () => {
+  test('Testa se, ao encontrar um resultado válido, os elementos são exibidos na tela', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => { history.push('/meals'); });
 
+    const nameRadio = screen.getByTestId('name-search-radio');
+    expect(nameRadio).toBeInTheDocument();
+    userEvent.click(nameRadio);
+
+    const searchBtn = screen.getByTestId('button-search');
+    expect(searchBtn).toBeInTheDocument();
+    userEvent.click(searchBtn);
+
+    const searchInput = screen.getByTestId('search-input');
+    expect(searchInput).toBeInTheDocument();
+    userEvent.type(searchInput, 'Corba');
+
+    const execBtn = screen.getByTestId('exec-search-btn');
+    expect(execBtn).toBeInTheDocument();
+    userEvent.click(execBtn);
   });
 });
